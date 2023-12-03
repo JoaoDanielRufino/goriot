bcaab7446f1e3ea84ac53bc48f04f93aaa4da2ed
JoaoDanielRufino <joaodaniel0405@gmail.com>
2023-12-03 16:27:57 -0300

    refactor riot constants

---
+++ riot/lol/constants.go
@@ -0,0 +1,23 @@
+package lol
+
+const (
+	RankedSolo5x5Queue = "RANKED_SOLO_5x5"
+	RankedFlexSRQueue  = "RANKED_FLEX_SR"
+	RankedFlexTTQueue  = "RANKED_FLEX_TT"
+
+	IronTier        = "IRON"
+	BronzeTier      = "BRONZE"
+	SilverTier      = "SILVER"
+	GoldTier        = "GOLD"
+	PlatinumTier    = "PLATINUM"
+	EmeraldTier     = "EMERALD"
+	DiamondTier     = "DIAMOND"
+	MasterTier      = "MASTER"
+	GrandMasterTier = "GRANDMASTER"
+	ChallengerTier  = "CHALLENGER"
+
+	DivisionOne   = "I"
+	DivisionTwo   = "II"
+	DivisionThree = "III"
+	DivisionFour  = "IV"
+)
